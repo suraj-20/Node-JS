c9fdc6c450ec8f6ca6f80af1015622dbad1abeff
Suraj <kumarsuraj54057@gmail.com>
2024-01-12 21:58:58 +0530

    Midlewares

--- PROJECT-01/index.js
+++ PROJECT-01/index.js
@@ -1,7 +1,27 @@
 const express = require("express");
 const users = require("./MOCK_DATA.json");
+const fs = require("fs");
 const app = express();
 const PORT = 8000;
+
+// Middleware - plugin (We can use body-parse.json() and express.json() in place of express.urlencoded)
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  console.log("Midleware 1");
+  next();
+});
+
+app.use((req, res, next) => {
+  const d = Date.now();
+  fs.appendFile(
+    "log.txt",
+    `\n${d.toString()}: ${req.ip}: ${req.method}: ${req.path}`,
+    (err, data) => {
+      next();
+    }
+  );
+});
 
 app.get("/users", (req, res) => {
   const html = `
@@ -21,21 +41,45 @@ app.get("/api/users", (req, res) => {
 app
   .route("/api/users/:id")
   .get((req, res) => {
-    const id = Number(req.params.id);   // convert string into number using Number keyword.
+    const id = Number(req.params.id); // convert string into number using Number keyword.
     const user = users.find((user) => user.id === id);
+    // console.log(user);
 
     return res.json(user);
   })
   .patch((req, res) => {
-    return res.json({ status: "Pending" });
+    const id = Number(req.params.id);
+    const userBody = req.body;
+    const userID = users.findIndex((user) => user.id === id);
+    // console.log("userID :", userID);
+
+    const getUser = users[userID];
+    const updated_User = { ...getUser, ...userBody };
+
+    users[userID] = updated_User;
+
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+      return res.send({ status: "success", updated_User });
+    });
   })
   .delete((req, res) => {
-    return res.json({ status: "Pending" });
+    const id = Number(req.params.id);
+    const userID = users.findIndex((user) => user.id === id);
+    console.log("userID :", userID);
+    const del_User = users.splice(userID, 1);
+
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+      return res.json({ status: "success", del_User });
+    });
   });
 
-
 app.post("/api/users", (req, res) => {
-  return res.json({ status: "pending" });
+  const body = req.body;
+  //   console.log("body", body);
+  users.push({ ...body, id: users.length + 1 });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: "success", id: users.length });
+  });
 });
 
 app.listen(PORT, () => {
